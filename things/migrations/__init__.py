f650fc37b7498f32fdf867b31dca56b3d409cbf7
SifaShaikh <114479739+SifaShaikh@users.noreply.github.com>
2022-12-09 11:01:49 +0000

    created the home html page, added to migrations and changed home in views.py

---
+++ things/migrations/__init__.py
@@ -0,0 +1,23 @@
+import django.core.validators
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Thing',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=35, unique=True)),
+                ('description', models.CharField(blank=True, max_length=120)),
+                ('quantity', models.IntegerField(validators=[django.core.validators.MinValueValidator(0), django.core.validators.MaxValueValidator(50)])),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
